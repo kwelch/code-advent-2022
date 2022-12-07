c2c345b8ed8cd59211f7a7b2a29554c388bed4de
Kyle Welch <1295580+kwelch@users.noreply.github.com>
2022-12-07 17:51:25 -0600

    --wip-- [skip ci]

--- examples/07/main.rs
+++ examples/07/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, fs::read_to_string};
 
+use itertools::Itertools;
+
 #[derive(Clone)]
 struct Directory<'a> {
     files: Vec<usize>,
@@ -25,33 +27,76 @@ impl<'a> Directory<'a> {
 
         total_size + dir_total
     }
+
+    fn add_file(&mut self, size: usize) {
+        self.files.push(size);
+    }
+
+    fn add_dir(&mut self, dir: &Directory<'a>) {
+        self.dirs.push(dir);
+    }
 }
 
 fn part_one(_contents: &String) -> usize {
-    let dir_stack: Vec<&Directory> = vec![];
-
     let max_size = 100000;
-    let e = Directory {
-        files: vec![584],
-        dirs: vec![],
-    };
-    let a = Directory {
-        files: vec![29116, 2557, 62596],
-        dirs: vec![&e],
-    };
-    let d = Directory {
-        files: vec![4060174, 8033020, 5626152, 7214296],
-        dirs: vec![],
-    };
-    let top = Directory {
-        files: vec![14848514, 8504156],
-        dirs: vec![&a, &d],
-    };
+    let mut dir_stack: Vec<&str> = vec![];
+    let mut all_dirs: HashMap<&str, Directory> = HashMap::new();
 
-    top.dirs
+    for line in _contents.lines() {
+        match line.split_once(" ").unwrap() {
+            ("$", command) => {
+                match command {
+                    // no details from this line skip
+                    "ls" => {
+                        println!("{:?}", dir_stack.last().unwrap());
+                    }
+                    command if command.starts_with("cd") => {
+                        let (_, dir_name) = command.split_once(" ").unwrap();
+                        if dir_name == ".." {
+                            dir_stack.pop();
+                        } else {
+                            all_dirs.insert(dir_name, Directory::new());
+                            dir_stack.push(dir_name)
+                        }
+                    }
+                    _ => println!("Unknown command: {}", command),
+                }
+            }
+            (size_str, _) if usize::from_str_radix(size_str, 10).is_ok() => {
+                let size  = usize::from_str_radix(size_str, 10).unwrap();
+                let dir = all_dirs.get(dir_stack.last().unwrap()).unwrap();
+                dir.add_file(size);
+            }
+            _ => println!("Don't know have to handle: {}", line),
+        }
+    }
+
+    println!("{:?}", dir_stack);
+
+    // all_dirs.insert("e", &Directory {
+    //     files: vec![584],
+    //     dirs: vec![],
+    // });
+
+    // let dir = Directory {
+    //     files: vec![29116, 2557, 62596],
+    //     dirs: vec![all_dirs.get("e").unwrap()],
+    // };
+    // all_dirs.insert("a", &dir);
+
+    // all_dirs.insert("d", Directory {
+    //     files: vec![4060174, 8033020, 5626152, 7214296],
+    //     dirs: vec![],
+    // });
+    // all_dirs.insert("top", Directory {
+    //     files: vec![14848514, 8504156],
+    //     dirs: vec![all_dirs.get("a").unwrap(), &all_dirs.get("d").unwrap()],
+    // });
+
+    all_dirs
         .into_iter()
-        .filter(|dir| dir.get_size() <= max_size)
-        .map(|dir| dir.get_size())
+        .filter(|(_name, dir)| dir.get_size() <= max_size)
+        .map(|(_name, dir)| dir.get_size())
         .sum()
 }
 
